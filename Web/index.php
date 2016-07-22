b65bf5f58611f9830fde0cfe9409a80e47bd20a6
AlphaLaboratory <mangportal@bk.ru>
2016-07-22 02:07:33 +0000

    Update index.php

--- Web/index.php
+++ Web/index.php
@@ -1,14 +1,15 @@
 <?php
-
 /**
  * Example file for Web src
- * @author: Alpha Lab | AL
- * @since: 22.07.2016 | 11:20
+ * @author: Alpha Lab
+ * @since: 22.07.2016
  */
-namespace App;
+ 
+namespace GB;
+use GB\App\GData\Defines as Defines;
 
-RoutingManager::GetInstance()->StartSurf(SAFE_MODE);
-SafeManager::GetInstance()->SetListenSwitch();
-SafeManager::GetInstance()->SetTypeSZones(RU, "GMT+12");
-CacheManager::GetInstance()->ClearCache(12);
+\GB\App\Router\RoutingManager::GetInstance()->StartSurf(\Defines\SAFE_MODE);
+\GB\App\Security\SafeManager::GetInstance()->SetListenSwitch();
+\GB\App\Security\SafeManager::GetInstance()->SetTypeSZones(RU, "GMT+12");
+\GB\App\Misc\CacheManager::GetInstance()->ClearCache(12);
 ?>
